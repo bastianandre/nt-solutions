a084efd402e7c32258c2499327b2fed994f8f394
Bastian André <bastian.andre@enso-gruppe.de>
2025-03-30 15:25:48 +0200

    move dist to root

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import './assets/styles/index.scss'
-
-createApp(App).use(router).use(createPinia()).mount('#app')
